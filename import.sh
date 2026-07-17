e03e42f9420538996a8ea038322827d23a40086d
Massimo Monai <113710214+maxm33@users.noreply.github.com>
2026-07-17 06:03:11 +0200

    Create import.sh

---
+++ import.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -e
+
+USER_HOME="$USERPROFILE"
+
+SOURCE="$USER_HOME/Desktop/remote/saves/EldenRing/76561197960271872"
+DEST="$USER_HOME/AppData/Roaming/EldenRing/76561197960271872"
+
+FILES=(
+    "ER0000.sl2"
+    "ER0000.sl2.bak"
+)
+
+echo
+echo "Checking savefile timestamps..."
+echo
+
+for FILE in "${FILES[@]}"; do
+    file_src="$SOURCE/$FILE"
+    file_dst="$DEST/$FILE"
+
+    if [[ -f "$file_src" && -f "$file_dst" ]]; then
+        if [[ "$file_dst" -nt "$file_src" ]]; then
+            echo "Aborted: '$FILE' in local destination is newer than the remote source."
+            exit 1
+        fi
+    fi
+done
+
+echo "Copying savefiles from remote to local..."
+echo
+
+cp -rf "$SOURCE/"* "$DEST/"
+
+echo
+echo "Done."
