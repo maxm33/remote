703bea8fa306bc5d325b88da13fdd9030e7f3ce2
maxm33 <monai.massi@gmail.com>
2026-07-17 05:23:50 +0200

    000260

--- backup.sh
+++ backup.sh
@@ -8,7 +8,7 @@ SOURCE="$USER_HOME/AppData/Roaming/EldenRing/76561197960271872"
 DEST="$USER_HOME/Desktop/remote/saves/76561197960271872"
 REPO="$USER_HOME/Desktop/remote"
 
-echo "Copia dei salvataggi..."
+echo -n "Copia dei salvataggi..."
 
 cp -rf "$SOURCE/"* "$DEST/"
 
@@ -18,8 +18,8 @@ git add --all
 
 COMMIT_MSG=$(printf "%06d" $(( RANDOM % 1000000 )))
 
-git commit -m "$COMMIT_MSG"
+git commit -m "$COMMIT_MSG" --quiet
 
-git push
+git push --quiet
 
-echo "Operazione completata."
+echo -n "Operazione completata."
